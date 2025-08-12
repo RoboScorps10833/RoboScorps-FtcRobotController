5d3c3bf619b921cefa623ba9b2861cd95cf7966e
nerd <pearl.icloud3@gmail.com>
2025-08-12 13:08:20 -0400

    Experimented with Vision
    NOTES:
    Checked out the vision features the SDK has. It has object recongition, color definition, and the ability to use apriltags wrapped nicely in the SDK class VisionPortral.
    Added examples that might prove useful for next season.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/ProductionAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/ProductionAutoOpMode.java
@@ -33,6 +33,20 @@ public class ProductionAutoOpMode extends OpMode {
 
         // Put vision initialization here
 
+        /*
+         * TODO: Implement Visual Localization (Extra)
+         * TODO: Convert Camera location to RR Pose2d (Extra)
+         * There's a tutorial for getting global pose for your robot by reading the field's
+         * AprilTags on the walls.
+         *
+         * https://ftc-docs.firstinspires.org/en/latest/apriltag/vision_portal/apriltag_localization/apriltag-localization.html#introduction
+         *
+         * When the season starts, I would like to test this out, but this isn't really necessary.
+         * It's very nice for roadrunner, and figuring out where the global position of the robot
+         * is automatically, and makes us flexible in competition, but then again we can manually
+         * code it.
+         */
+
         // Trajectory definition manual entry...
             // Red and left
             // Red and right
