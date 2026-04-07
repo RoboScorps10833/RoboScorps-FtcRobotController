a3a169e050d1e21b2bcf7eda61348110058c79b5
Minghua <mingwei.chg@gmail.com>
2026-04-07 16:21:53 -0400

    add motors
    NOTES:
    - NEW: /Mechanisms.
    - NEW: MecanumDrive class. Declared Motors.
    To align with joysticks Tomorrow.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/MecanumDrive.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class MecanumDrive extends OpMode {
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
+
+    public void moveDriveTrain(){
+        double vertical;
+        double horizontal;
+        double pivot;
+
+    }
+
+
+    public void init() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class,"frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class,"frontRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class,"backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class,"backRightMotor");
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+}
+
+
