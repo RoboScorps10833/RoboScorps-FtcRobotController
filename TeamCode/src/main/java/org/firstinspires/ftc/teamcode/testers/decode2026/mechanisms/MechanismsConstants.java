0e2f96549eba1878e318155ba50f3883536f2dad
Minghua <mingwei.chg@gmail.com>
2026-04-23 18:02:11 -0400

    Add Sliders mechanism class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/decode2026/mechanisms/MechanismsConstants.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.testers.decode2026.mechanisms;
-
-
-/**
- * A singlar file to throw all of the constants in since they didn't really fit anywhere else.
- * All Constants will be applied and accessed in the Programming Board.
- */
-public class MechanismsConstants {
-}
